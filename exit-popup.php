634c3f95d22f79aa9fc05611dda5c2045eee50df
Robert O'Rourke <robert@o-rourke.org>
2024-01-12 13:01:47 +0000

    Defer script loading

--- exit-popup.php
+++ exit-popup.php
@@ -13,6 +13,8 @@
  * @package           exit-popup
  */
 
+namespace HM\Exit_Popup;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -24,8 +26,18 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function exit_popup_exit_popup_block_init() {
+function init() {
 	register_block_type( __DIR__ . '/build' );
 }
 
-add_action( 'init', 'exit_popup_exit_popup_block_init' );
+add_action( 'init', __NAMESPACE__ . '\\init' );
+
+/**
+ * Fires when scripts and styles are enqueued.
+ *
+ */
+function enqueue_scripts() : void {
+	wp_script_add_data( 'hm-exit-popup-view-script', 'strategy', 'defer' );
+}
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
